5a401855d867fb87f9af6daa89272160d60e4cf0
alexandrakurowski <alexandrakuro@gmail.com>
2019-03-03 22:13:13 +0100

    essai carte

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -41,4 +41,36 @@ function w3_close() {
     document.getElementById("openNav").style.display = "inline-block";
 }
 
+/*****************************************************************************************************************************************************************************************************************************************************************
+|
+|    ALL THE FUNCTIONS TO GET AND INITIALIZE THE MAP with leaflet
+ *******************************************************************************
+ */
+
+// console.log("hello mymapleaflet");
+
+// //AUCH MAP INTIALIZING [lat, long, zoomsize]
+// var mymap = L.map('mapid').setView([0, 586709, 43, 64638], 13);
+
+// // ACCESS TOKEN MAPBOX AND NAME OF THE MAP
+
+// L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     id: 'mapbox.streets',
+//     accessToken: 'pk.eyJ1Ijoia2FjcmVhdGlvbiIsImEiOiJjanJrczE4cDIwMHhzNDVtcDhzdTRraGRvIn0.5fEOyjYgkK_BrlPPp2xJXg'
+// }).addTo(mymap);
+
+/* Map with MAPBOX ************************************************
+*/
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoia2FjcmVhdGlvbiIsImEiOiJjanJrczE4cDIwMHhzNDVtcDhzdTRraGRvIn0.5fEOyjYgkK_BrlPPp2xJXg';
+var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
+    center: [43, 64638, 0, 586709], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+});
+
+
 
