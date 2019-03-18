c5ecc01b611230ea7b4f886901142fdc75c7b0e4
alexandrakurowski <alexandrakuro@gmail.com>
2019-03-18 10:44:34 +0100

    init DB  + fill DB

--- platforms/browser/www/js/initDb.js
+++ platforms/browser/www/js/initDb.js
@@ -5,8 +5,7 @@ var db = window.openDatabase("database", "1.0", "Cordova Demo", 200000);
 var allPartners;
 var allCategories;
 
-//cette fonction importe les données des fichiers json grâce à des requêtes ajax et éxécute les fonctions
-//qui remplissent les tables
+//this function get datas from jsonend fill the dataBase
 function fillDB(tx) {
     console.log("je remplie la db");
 
@@ -19,6 +18,16 @@ function fillDB(tx) {
         allCategories = data1;
         db.transaction(fillCategories, errorCB, successCB);
     });
+    $.ajax({
+        type: "get",
+        url: "json/partenaires08_03.json",
+        dataType: "json"
+    }).done(function (data) {
+        console.log(data)
+        allPartners = data;
+        db.transaction(fillPartners, errorCB, successCB);
+    });
+
 }
 // funtion error or success for fill db
 function errorCB(err) {
@@ -39,6 +48,17 @@ function fillCategories(tx) {
     for (var i = 0; i < allCategories.length; i++) {
         console.log("je boucle dans categories");
         var sql = 'INSERT INTO categories (category_id, category_name,fk_markers) VALUES ('
+            + allCategories[i].category_id + ', "' + allCategories[i].category_name + '", "' + allCategories[i].fk_markers + '")';
+        tx.executeSql(sql);
+    }
+}
+function fillPartners(tx) {
+    console.log("testfillCate");
+    tx.executeSql("DROP TABLE IF EXISTS partners");
+    tx.executeSql("CREATE TABLE IF NOT EXISTS partners (partner_id unique, name_ets, tel_ets, mail_ets, site_ets, contact_name, description_ets, image_ets, fk_adress, fk_offer)");
+    for (var i = 0; i < allPartners.length; i++) {
+        console.log("je boucle dans partners");
+        var sql = 'INSERT INTO partners (partner_id unique, name_ets, tel_ets, mail_ets, site_ets, contact_name, description_ets, image_ets, fk_adress, fk_offer) VALUES ('
             + allCategories[i].category_id + ', "' + allCategories[i].category_name + '", "' + allCategories[i].fk_markers + '")';
         tx.executeSql(sql);
     }
