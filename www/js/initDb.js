e2e475459af37f2e1f356ef0e0e9add5696c8439
alexandrakurowski <alexandrakuro@gmail.com>
2019-03-27 09:34:08 +0100

    OK LOCAL DB

--- www/js/initDb.js
+++ www/js/initDb.js
@@ -4,8 +4,9 @@ var db = window.openDatabase("database", "1.0", "Cordova Demo", 200000);
 
 var allPartners;
 var allCategories;
+var allEmployee;
 
-//this function get datas from json and fill the dataBase
+//this function fill all tables of the db
 function fillDB(tx) {
     console.log("start db filding");
 
@@ -14,7 +15,7 @@ function fillDB(tx) {
         url: "json/categories.json",
         dataType: "json"
     }).done(function (data1) {
-        console.log(data1)
+        console.log(data1);
         allCategories = data1;
         db.transaction(fillCategories, errorCB, successCB);
     });
@@ -23,7 +24,7 @@ function fillDB(tx) {
         url: "json/partenaires08_03.json",
         dataType: "json"
     }).done(function (data) {
-        console.log(data)
+        console.log(data);
         allPartners = data;
         db.transaction(fillPartners, errorCB, successCB);
         db.transaction(fillOffers, errorCB, successCB);
@@ -31,7 +32,17 @@ function fillDB(tx) {
         db.transaction(fillContacts, errorCB, successCB);
 
     });
-
+    $.ajax({
+        type: "get",
+        url: "json/employee.json",
+        dataType: "json"
+    }).done(function (data2) {
+        console.log(data2),
+            allEmployee = data2;
+        db.transaction(fillEmployee, errorCB, successCB);
+        // CAREFUL NEED TO FIND SOLUTION TO FILL USER WITH AJX REQUEST FROM PARTNERS AND EMPLOYEE IN NEXT VERSION OF MY GEGG
+        db.transaction(fillUsers, errorCB, successCB);
+    });
 }
 // funtion error or success for fill db
 function errorCB(err) {
@@ -44,7 +55,7 @@ function successCB() {
 
 
 
-//function to fill all tables of database 
+//functions to fill each tables of database 
 function fillCategories(tx) {
     console.log("testfillCate");
     tx.executeSql("DROP TABLE IF EXISTS categories");
@@ -56,7 +67,7 @@ function fillCategories(tx) {
         tx.executeSql(sql);
     }
 }
-//fill  all tables  with datas Json
+
 function fillPartners(tx) {
     console.log("testfillPartners");
     tx.executeSql("DROP TABLE IF EXISTS partners");
@@ -83,7 +94,7 @@ function fillAdresses(tx) {
     tx.executeSql("DROP TABLE IF EXISTS adresses");
     tx.executeSql("CREATE TABLE IF NOT EXISTS adresses(street_number, street_name, town, postal_code, lat, long, fk_partners)");
     for (var i = 0; i < allPartners.length; i++) {
-        console.log("loop into adresses")
+        console.log("loop into partners/adresses")
         var sql = 'INSERT INTO adresses(street_number, street_name, town, postal_code, lat, long, fk_partners) VALUES("' + allPartners[i].numeroRue + '","' + allPartners[i].nomRue + '", "' + allPartners[i].ville + '", "' + allPartners[i].codePostal + '", "' + allPartners[i].lat + '", "' + allPartners[i].long + '", "' + allPartners[i].fk_partners + '")';
         tx.executeSql(sql);
     }
@@ -93,8 +104,30 @@ function fillContacts(tx) {
     tx.executeSql("DROP TABLE IF EXISTS contacts");
     tx.executeSql("CREATE TABLE IF NOT EXISTS contacts(contact_name, contact_role, contact_mail, contact_tel, fk_partners)");
     for (var i = 0; i < allPartners.length; i++) {
-        console.log("loop into contact");
+        console.log("loop into partners/contact");
         var sql = 'INSERT INTO contacts (contact_name, contact_role, contact_mail, contact_tel, fk_partners) VALUES ("' + allPartners[i].nomContact + '", "' + allPartners[i].roleContact + '", "' + allPartners[i].mailContact + '", "' + allPartners[i].telContact + '", "' + allPartners[i].fk_partners + '")';
         tx.executeSql(sql);
     }
 }
+function fillEmployee(tx) {
+    console.log("test fill employee");
+    tx.executeSql("DROP TABLE IF EXISTS employee");
+    tx.executeSql("CREATE TABLE IF NOT EXISTS employee(id_gegg, employee_name, employee_firstName, employee_mail, admin)");
+    for (var i = 0; i < allEmployee.length; i++) {
+        console.log("loop into employee");
+        var sql = 'INSERT INTO employee(id_gegg, employee_name, employee_firstName, employee_mail, admin) VALUES (' + allEmployee[i].employee_id + ',' + allEmployee[i].gegg_id + ', "' + allEmployee[i].name_employee + '","' + allEmployee[i].firstName_employee + '","' + allEmployee[i].mail_employee + '","' + allEmployee[i].admin + '")';
+        tx.executeSql(sql);
+    }
+}
+// A REPRENDRE AVEC LA FONCTION S'INSCRIRE QUI VA REMPLIR LES CHAMPS
+//
+// function fillUsers(tx) {
+//     console.log(" test function fill user");
+//     tx.executeSql("DROP TABLE IF EXISTS users");
+//     tx.executeSql("CREATE TABLE IF NOT EXISTS users(user_pwd, user_mail, fk_employee)");  // BEWARE no fk_partners init in V1
+//     for (var i = 0; i < allEmployee.length; i++){
+//         console.log("loop into employee ");
+//         var sql = 'INSERT INTO users (user_pwd, user_mail)'
+//     }
+// }
+
